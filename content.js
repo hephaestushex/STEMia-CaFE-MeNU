290660bd21484adc20fe5a4678232a72bf85ae4f
Le Duck <95136800+duck123acb@users.noreply.github.com>
2022-03-17 20:49:02 -0600

    removed commented out code

--- content.js
+++ content.js
@@ -1,16 +1,3 @@
-// const breakfastMoreInfo = document.getElementById("breakfast-more-info");
-// const breakfastInfo = document.getElementById("breakfast-info");
-
-// breakfastInfo.style.display = "none";
-
-// breakfastMoreInfo.addEventListener("click", function () {
-// 	if (breakfastInfo.style.display === "none") {
-// 		breakfastInfo.style.display = "block";
-// 	} else if (breakfastInfo.style.display === "block") {
-// 		breakfastInfo.style.display = "none";
-// 	}
-// });
-
 const moreInfoButtons = document.querySelectorAll(".more-info");
 const infoTexts = document.querySelectorAll(".info");
 console.log(infoTexts)
@@ -18,19 +5,6 @@ console.log(infoTexts)
 infoTexts.forEach(infoText => {
 	infoText.style.display = "none";
 });
-
-// moreInfoButtons.forEach(moreInfoButton => {
-// 	moreInfoButton.addEventListener("click", function () {
-// 		//infoTexts.forEach(infoText => {
-// 		if (infoTexts[0].style.display === "none") {
-// 			infoTexts[0].style.display = "block";
-// 		} else if (infoTexts[0].style.display === "block") {
-// 			infoTexts[0].style.display = "none";
-// 		}
-// 		//});
-// 	});
-// });
-/****** not gonna use foreach ******/
 
 for (let i = 0; i < moreInfoButtons.length; i++) {
 	moreInfoButtons[i].addEventListener("click", function () {
